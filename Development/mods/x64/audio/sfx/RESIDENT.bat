75d6522aaa01428f7f4fd7a57b463ee5b2f3d4e2
Waynieoaks <5962977+waynieoaks@users.noreply.github.com>
2021-01-10 11:04:20 +0000

    1.0.1
    include audio from resident.rpf
    remove duplicate audio files ..wav

---
+++ Development/mods/x64/audio/sfx/RESIDENT.bat
@@ -0,0 +1,14 @@
+copy /Y null.wav RESIDENT\frontend\0x05CA67D5.wav
+copy /Y null.wav RESIDENT\frontend\0x074729F7.wav
+copy /Y null.wav RESIDENT\frontend\0x0826AC8E.wav
+copy /Y null.wav RESIDENT\frontend\0x0DEFB827.wav
+copy /Y null.wav RESIDENT\frontend\0x0FE5FB3B.wav
+copy /Y null.wav RESIDENT\frontend\0x1018C2CC.wav
+copy /Y null.wav RESIDENT\frontend\0x10E0BD2B.wav
+copy /Y null.wav RESIDENT\frontend\0x17730A4F.wav
+copy /Y null.wav RESIDENT\frontend\0x18F84D60.wav
+copy /Y null.wav RESIDENT\frontend\0x1BA0D2AA.wav
+copy /Y null.wav RESIDENT\frontend\0x1D745657.wav
+copy /Y null.wav RESIDENT\frontend\0x1E941898.wav
+copy /Y null.wav RESIDENT\frontend\0x1F751A54.wav
+PAUSE
